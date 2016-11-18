d22f2865ae7afb29f0344ae1162439d0f16628c2
Ottavio Hartman <hartmano@sas.upenn.edu>
2016-11-18 17:49:40 -0500

    Starting to implement textures

--- js/particleRender.js
+++ js/particleRender.js
@@ -7,7 +7,14 @@
 			return;
 		}
 		
+		// Collision
+		// Bind collision shaders, bind position + vel textures -> write to force texture
+		
 		// Render
+		// Bind render shaders, bind position texture -> vertex shader transforms particles to new positions
+		
+		// Update state
+		// Bind update shaders, bind force texture -> write to velocity and position texture
 		renderParticles(R.progParticle);
     };
     
@@ -26,6 +33,9 @@
 		var m = state.cameraMat.elements;
 		gl.uniformMatrix4fv(prog.u_cameraMat, false, m);
 		
+		// Bind textures
+		bindTextures(prog);
+		
 		gl.clearColor(0.5, 0.5, 0.5, 0.9);
         
 		gl.enable(gl.DEPTH_TEST);
@@ -33,6 +43,17 @@
 		gl.clear(gl.COLOR_BUFFER_BIT);
 
 		gl.drawArrays(gl.POINTS, 0, R.sphereModel.elemCount);
+	}
+	
+	var bindTextures = function(prog) {
+		// Use the program
+		gl.useProgram(prog.prog);
+		
+		gl.activeTexture(gl['TEXTURE0']);
+		
+        gl.bindTexture(gl.TEXTURE_2D, R.positionTex);
+		
+        gl.uniform1i(prog.u_pos, 0);
 	}
 
 })();
